93080032be2fb853e88ab1786a938708d0fe9ec5
taly <hectoivanaguilar23@gmail.com>
2023-09-29 03:35:38 -0300

    agrege la API

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    
+    SERVER_NAME = "127.0.0.1:5000"
+    DEBUG = True
+
+    # Ruta donde se almacenarán las imágenes de perfil
+    UPLOAD_FOLDER = 'imagenes'
+    TEMPLATE_FOLDER = "templates/"
+    STATIC_FOLDER = "static/"
